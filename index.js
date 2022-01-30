0746a5905df05f4b28ca4cf778be1dab36fa1168
supreeth avadhani <supreethavadhani@gmail.com>
2022-01-29 21:43:57 -0800

    added token auth

--- index.js
+++ index.js
@@ -1,81 +1,116 @@
 const express = require('express')
-const moment= require('moment') 
+const moment = require('moment')
 const app = express()
 const port = 3000
 var bodyParser = require('body-parser')
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
-})); 
+}));
 
 app.get('/v1/weather', (req, res) => {
-  res.send({
-    "coord": {
+  let reqToken = getToken(req);
+  if (!reqToken) {
+    res.status(400)
+    res.send({
+      "response": "Token not found"
+    })
+  } else if (reqToken != token) {
+    res.status(401)
+    res.send({
+      "response": "Unauthorized"
+    })
+  } else {
+    res.send({
+      "coord": {
         "lon": -123.262,
         "lat": 44.5646
-    },
-    "weather": [
-        {
-            "id": 804,
-            "main": "Clouds",
-            "description": "overcast clouds",
-            "icon": "04d"
-        }
-    ],
-    "base": "stations",
-    "main": {
+      },
+      "weather": [{
+        "id": 804,
+        "main": "Clouds",
+        "description": "overcast clouds",
+        "icon": "04d"
+      }],
+      "base": "stations",
+      "main": {
         "temp": 282.37,
         "feels_like": 282.09,
         "temp_min": 281.16,
         "temp_max": 283.41,
         "pressure": 1020,
         "humidity": 78
-    },
-    "visibility": 10000,
-    "wind": {
+      },
+      "visibility": 10000,
+      "wind": {
         "speed": 1.34,
         "deg": 135,
         "gust": 2.24
-    },
-    "clouds": {
+      },
+      "clouds": {
         "all": 100
-    },
-    "dt": 1642095101,
-    "sys": {
+      },
+      "dt": 1642095101,
+      "sys": {
         "type": 2,
         "id": 2040223,
         "country": "US",
         "sunrise": 1642088828,
         "sunset": 1642121762
-    },
-    "timezone": -28800,
-    "id": 5720727,
-    "name": "Corvallis",
-    "cod": 200
-})
+      },
+      "timezone": -28800,
+      "id": 5720727,
+      "name": "Corvallis",
+      "cod": 200
+    })
+  }
 })
 
 app.get('/v1/hello', (req, res) => {
-    res.send({ "Greetings":"Hello world"})
-  })
-
-  app.post('/v1/auth', (req, res) => {
-    let username = req.body.username
-    let password = req.body.password
-    if(!username || !password){
-        res.status(400);
-        res.send({"response":"Password or username empty"})
-    }
-    else{
-        res.send({
-          "access-token": token,
-          "expires": moment().add(30, 'm').toDate()
-        })
-      }
+  let reqToken = getToken(req);
+  if (!reqToken) {
+    res.status(400)
+    res.send({
+      "response": "Token not found"
     })
+  } else if (reqToken != token) {
+    res.status(401)
+    res.send({
+      "response": "Unauthorized"
+    })
+  } else {
+    res.send({
+      "Greetings": "Hello world"
+    })
+  }
+})
+
+app.post('/v1/auth', (req, res) => {
+  let username = req.body.username
+  let password = req.body.password
+  if (!username || !password) {
+    res.status(400);
+    res.send({
+      "response": "Password or username empty"
+    })
+  } else {
+    res.send({
+      "access-token": token,
+      "expires": moment().add(30, 'm').toDate()
+    })
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+function getToken(req) {
+  var header = req.headers['authorization']
+  if (header) {
+    let token = header.split(' ')[1]
+    return(token)
+  }
+}
