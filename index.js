d6437482a4dff8ab55a1a555feeaa3261effb026
supreeth avadhani <supreethavadhani@gmail.com>
2022-02-04 13:29:04 -0800

    bug fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log('Node. js Express server is running on port 3000... ')
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    const allowedOrigins = ['https://editor.swagger.io', 'https://hoppscotch.io','http://ec2-34-207-116-86.compute-1.amazonaws.com:3000'];
+    const allowedOrigins = ['https://editor.swagger.io', 'https://hoppscotch.io','http://ec2-34-207-116-86.compute-1.amazonaws.com:3000','http://ec2-34-207-116-86.compute-1.amazonaws.com:3002'];
     const origin = req.headers.origin;
     
     if (allowedOrigins.includes(origin)) {
